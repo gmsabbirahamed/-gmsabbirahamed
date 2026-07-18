e14fd7d139b3e7bc7d814e3589afbc7466542a9c
Shawon <75561990+sheikhshawon@users.noreply.github.com>
2026-07-18 19:40:54 +0600

    Add files via upload

---
+++ src/data/home.ts
@@ -0,0 +1,18 @@
+// ==================[ Personal Data ]================== //
+export const personal_data = {
+  short_description:
+    "“I design sleek, high—performance web experience that help brands stand out.”",
+
+  name: "Thomas Walker", // must write first name + last name
+
+  image: "/images/hero.jpg",
+
+  job: "Brand & Freelancer Website Designer",
+
+  location: "Based in New York, United States",
+} as const;
+//
+
+// ==================[ Spinning Text ]================== //
+export const spinning_text_words = "hire me ✺ hire me ✺ hire me ✺ hire me ✺ ";
+//
